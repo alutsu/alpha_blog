6f6a390433c168b401ad260a9b5d3a80a27c2002
João Victor <jovictor.miguel@gmail.com>
2019-05-07 10:33:32 -0300

    Add permissions rules and authenticate system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 1)
@@ -40,5 +41,11 @@ class UsersController < ApplicationController
     end
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+    def require_same_user
+      if !logged_in? || current_user != @user
+        flash[:danger] = "Você só pode editar sua propria conta"
+        redirect_to root_path
+      end
     end
 end
